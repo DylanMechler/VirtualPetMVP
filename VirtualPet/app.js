6c065f2160d40197de28fe7cfcad18f520cd7574
DylanMechler <27DMechler@live.ytech.edu>
2026-01-14 11:10:26 -0500

    Signup Logic Progress

--- VirtualPet/app.js
+++ VirtualPet/app.js
@@ -8,6 +8,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+const db = new sqlite3.Database('./data/database.sqlite', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the database.');
+});
+
 app.use(session({
     secret: 'secretString',
     resave: false,
@@ -29,13 +36,41 @@ app.get('/signup', (req, res) => {
 });
 
 app.post('/signup', (req, res) => {
-    var username = req.body.username;
-    var password = req.body.password;
-    if (!username || username === "" || !password || password === "") {
-        throw new Error("Username or Password Missing")
-    }
-    if (password.length < 8) {
-        throw new Error("Password Too Short")
+    try {
+        var username = req.body.username;
+        var password = req.body.password;
+        if (!username || username === "" || !password || password === "") {
+            throw new Error("Username or Password Missing")
+        }
+        if (password.length < 8) {
+            throw new Error("Password Too Short")
+        }
+        db.run(`INSERT INTO users(username, password) VALUES(?, ?)`, [username, password], function (err) {
+            try {
+                if (err) {
+                    console.log(err.message);
+                    throw new Error("Database insertion error");
+                }
+                console.log(`A row has been inserted with rowid ${this.lastID}`);
+                req.session.user = username;
+                db.get(`SELECT * FROM users WHERE username = ? AND password = ?`, [username, hashedPassword], (err, row) => {
+                    if (err) {
+                        throw err;
+                    }
+                    if (row) {
+                        req.session.userID = row.uid;
+                    } else {
+                        console.log("Error: Row Not Found After Signup");
+                        res.redirect('/');
+                    }
+                })
+                res.redirect('/');
+            } catch (error) {
+                res.redirect('/signup');
+            }
+        });
+    } catch (error) {
+        res.redirect('/signup');
     }
 });
 
