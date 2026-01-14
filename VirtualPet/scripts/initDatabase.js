a0f333fcce1271a802a452782b9ad8f0883f6aa7
DylanMechler <27DMechler@live.ytech.edu>
2026-01-14 08:57:32 -0500

    Init Database Script

---
+++ VirtualPet/scripts/initDatabase.js
@@ -0,0 +1,29 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('data/database.sqlite', (err) => {
+    if (err) {
+        console.error(`Error Connecting To Database: ${err.message}`);
+    } else {
+        console.log('Connected To Database');
+    }
+});
+
+db.run(`CREATE TABLE "users" (
+	"uid"	INTEGER NOT NULL UNIQUE,
+	"username"	TEXT NOT NULL UNIQUE,
+	"password"	TEXT NOT NULL,
+	PRIMARY KEY("uid" AUTOINCREMENT)
+)`, (err) => {
+    if (err) {
+        console.error(`Error Creating Users Table: ${err.message}`);
+    } else {
+        console.log("Users Table Created Successfully");
+    }
+    db.close((err) => {
+        if (err) {
+            console.error(`Error Closing Database: ${err.message}`);
+        } else {
+            console.log("Database Connection Closed");
+        }
+    });
+});
